5f43daa7d338dc199ab78db232bd6b89691ff887
PuppetVM <you@example.com>
2015-02-03 18:01:59 +0000

    multiple modules

--- modules/nginx1/manifests/init.pp
+++ modules/nginx1/manifests/init.pp
@@ -4,6 +4,14 @@
                 file { '/tmp/hello':
                         content => "Hello, world Changed by manifests and modules  operation ${timestamp}\n",
                         }   
- }
-
+      file { '/tmp/file1':
+         require => File['/tmp/file2'],
+            }
+      file { '/tmp/file2':
+        require => File['/tmp/file3'],
+           }
+      file { '/tmp/file3':
+        content => "Hello,  /tmp/file3 ${timestamp}\n",
+          }
+      }
 
