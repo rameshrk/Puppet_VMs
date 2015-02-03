38b2c9fda2c8a81629885ad6edb201eaaed858d6
PuppetVM <you@example.com>
2015-02-03 16:57:13 +0000

    multiple modules

--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -1,10 +1,19 @@
 
-
-
  class nginx {
 
                package { 'nginx':
-                            ensure => installed,
-               } 
-    
-  } 
+                        ensure => installed,
+                        require => Package['apache2.2-common'],
+                  }
+              
+                package { 'apache2.2-common':
+                             ensure => absent,
+                }
+
+
+              service { 'nginx':
+                      ensure  => running,
+                      require => Package['nginx'],
+                  }
+               
+         } 
