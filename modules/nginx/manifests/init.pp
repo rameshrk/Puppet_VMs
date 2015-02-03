29c566248003d07959d5fdda6509b2ee18e25ee0
PuppetVM <you@example.com>
2015-02-03 21:32:28 +0000

    Nginx running

--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -2,7 +2,7 @@
  class nginx {
 
                package { 'nginx':
-                        ensure => installed,
+                        ensure  => installed,
                         require => Package['apache2.2-common'],
                   }
               
@@ -12,8 +12,18 @@
 
 
               service { 'nginx':
-                      ensure  => running,
-                      require => Package['nginx'],
-                  }
+                      ensure    => running,
+                      require   => Package['nginx'],
+                      #enable    => false,
+                      #hasstatus => false,
+                      #pattern   => 'ping 127.0.0.1',
+                      #status    => 'grep nginx'
+
+                   }
+
+              file { '/etc/nginx/conf.d/default.conf':
+                      source     => 'puppet:///modules/nginx/cat-pictures.conf',
+                      notify => Service['nginx'],
+                 }
                
          } 
