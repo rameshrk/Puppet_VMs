5b2ca5478000b4e553365e1ef85c2733e6859a3f
PuppetVM <you@example.com>
2015-03-08 09:57:33 +1100

    exec more

--- modules/execs/manifests/init.pp
+++ modules/execs/manifests/init.pp
@@ -6,5 +6,10 @@
 
          exec { 'Run my second command':
            command => '/bin/echo I ran this second command on `/bin/date` >>/tmp/command.output.txt'
+          exec {  'Download test file':
+             cwd     => '/tmp',
+             command => '/usr/bin/wget www.google.com/humans.txt' 
+             creates => 'huamns_output.txt' 
+
 
 } }
