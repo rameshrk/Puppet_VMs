89adbc4d51deaeb671fa18acfb76f1c61afefd14
PuppetVM <you@example.com>
2015-03-08 11:25:26 +1100

    exec more

--- modules/execs/manifests/init.pp
+++ modules/execs/manifests/init.pp
@@ -6,10 +6,21 @@
 
          exec { 'Run my second command':
            command => '/bin/echo I ran this second command on `/bin/date` >>/tmp/command.output.txt'
-          exec {  'Download test file':
+           }
+           exec {  'Download test file':
              cwd     => '/tmp',
-             command => '/usr/bin/wget www.google.com/humans.txt' 
-             creates => 'huamns_output.txt' 
+             command => '/usr/bin/wget www.google.com/humans.txt' ,
+             creates => '/tmp/humans_output.txt' 
+          }
+         exec {  'Download test file again':
+             cwd     => '/tmp',
+             command => '/usr/bin/wget www.google.com/humans.txt >> /tmp/humans_output1.txt' ,
+             unless  => '/bin/grep /tmp/humans_output1.txt' 
+          }
 
-
-} }
+         exec {  'config check':
+            command     => 'bin/echo file got changed  on `/bin/date` >> /tmp/humans_monitor.txt' ,
+             refreshonly => true,
+             subscribe => File['/tmp/humans_output.txt']
+          }
+ }
